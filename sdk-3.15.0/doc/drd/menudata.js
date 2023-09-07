a38e9ec6a88fa98907e2b0d198ae8d29c86b4dbd
Sornsiri <127424423+psornsiri@users.noreply.github.com>
2023-09-07 17:46:00 +0100

    Add files via upload

---
+++ sdk-3.15.0/doc/drd/menudata.js
@@ -0,0 +1,94 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Related Pages",url:"pages.html"},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"auto",url:"globals.html#index_a"},
+{text:"chec",url:"globals.html#index_c"},
+{text:"clos",url:"globals.html#index_c"},
+{text:"enab",url:"globals.html#index_e"},
+{text:"getc",url:"globals.html#index_g"},
+{text:"getd",url:"globals.html#index_g"},
+{text:"gete",url:"globals.html#index_g"},
+{text:"getm",url:"globals.html#index_g"},
+{text:"getp",url:"globals.html#index_g"},
+{text:"gett",url:"globals.html#index_g"},
+{text:"getv",url:"globals.html#index_g"},
+{text:"hold",url:"globals.html#index_h"},
+{text:"isfi",url:"globals.html#index_i"},
+{text:"isin",url:"globals.html#index_i"},
+{text:"ismo",url:"globals.html#index_i"},
+{text:"isru",url:"globals.html#index_i"},
+{text:"issu",url:"globals.html#index_i"},
+{text:"lock",url:"globals.html#index_l"},
+{text:"move",url:"globals.html#index_m"},
+{text:"open",url:"globals.html#index_o"},
+{text:"regu",url:"globals.html#index_r"},
+{text:"setd",url:"globals.html#index_s"},
+{text:"sete",url:"globals.html#index_s"},
+{text:"setf",url:"globals.html#index_s"},
+{text:"setm",url:"globals.html#index_s"},
+{text:"setp",url:"globals.html#index_s"},
+{text:"slee",url:"globals.html#index_s"},
+{text:"star",url:"globals.html#index_s"},
+{text:"stop",url:"globals.html#index_s"},
+{text:"trac",url:"globals.html#index_t"},
+{text:"wait",url:"globals.html#index_w"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"auto",url:"globals_func.html#index_a"},
+{text:"chec",url:"globals_func.html#index_c"},
+{text:"clos",url:"globals_func.html#index_c"},
+{text:"enab",url:"globals_func.html#index_e"},
+{text:"getc",url:"globals_func.html#index_g"},
+{text:"getd",url:"globals_func.html#index_g"},
+{text:"gete",url:"globals_func.html#index_g"},
+{text:"getm",url:"globals_func.html#index_g"},
+{text:"getp",url:"globals_func.html#index_g"},
+{text:"gett",url:"globals_func.html#index_g"},
+{text:"getv",url:"globals_func.html#index_g"},
+{text:"hold",url:"globals_func.html#index_h"},
+{text:"isfi",url:"globals_func.html#index_i"},
+{text:"isin",url:"globals_func.html#index_i"},
+{text:"ismo",url:"globals_func.html#index_i"},
+{text:"isru",url:"globals_func.html#index_i"},
+{text:"issu",url:"globals_func.html#index_i"},
+{text:"lock",url:"globals_func.html#index_l"},
+{text:"move",url:"globals_func.html#index_m"},
+{text:"open",url:"globals_func.html#index_o"},
+{text:"regu",url:"globals_func.html#index_r"},
+{text:"setd",url:"globals_func.html#index_s"},
+{text:"sete",url:"globals_func.html#index_s"},
+{text:"setf",url:"globals_func.html#index_s"},
+{text:"setm",url:"globals_func.html#index_s"},
+{text:"setp",url:"globals_func.html#index_s"},
+{text:"slee",url:"globals_func.html#index_s"},
+{text:"star",url:"globals_func.html#index_s"},
+{text:"stop",url:"globals_func.html#index_s"},
+{text:"trac",url:"globals_func.html#index_t"},
+{text:"wait",url:"globals_func.html#index_w"}]}]}]}]}
